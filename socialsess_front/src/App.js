53d1652773023c8b0914eafda9f4be5079b6eba1
Jessica Im <me@jessicaim.com>
2021-11-17 12:07:35 -0500

    set up question mapping

--- socialsess_front/src/App.js
+++ socialsess_front/src/App.js
@@ -1,10 +1,30 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
      let [questions, setQuestions] = useState([])
+
+     const getQuestions = () => {
+          axios.get('http://localhost:8000/api/questions')
+          .then(
+               (response) => setQuestions(response.data)
+               (err) => console.error(error)
+          )
+          .catch((error) => console.error(error))
+     }
+
      return (
           <>
                <h1> hello universe </h1>
+               <div className="questions">
+                    {questions.map((question) => {
+                         return (
+                              <div className="question" key={question.id}>
+                                   <h4>{question.question}</h4>
+                              </div>
+                         )
+                    })}
+               </div>
           </>
      )
 }
