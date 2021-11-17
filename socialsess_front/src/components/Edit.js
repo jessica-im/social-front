ca8be5b6579eb1a1151b8560da8f89f715c1f314
Jessica Im <me@jessicaim.com>
2021-11-17 12:44:22 -0500

    set up edit component

---
+++ socialsess_front/src/components/Edit.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const Edit = (props) => {
+     const [question, setQuestion] = useState({ ...props.question })
+
+     const handleChange = (event) => {
+          setQuestion({ ...question, [event.target.name]: event.target.value })
+     }
+
+     const handleSubmit = (event) => {
+          event.preventDefault()
+          props.handleUpdate(question)
+     }
+
+     return (
+          <>
+               <details>
+                    <summary>Edit Question</summary>
+                    <form onSubmit={handleSubmit}>
+                         <label htmlFor="question">Question: </label>
+                         <input type="text" name="question" onChange={handleChange} value={question.question} />
+                         <br />
+                         <br />
+                         <input type="submit" />
+                    </form>
+               </details>
+          </>
+     )
+
+}
