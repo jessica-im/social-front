8c5cf59407a641a5c0b0476d9d977ccb5911b23a
Jessica Im <me@jessicaim.com>
2021-11-17 11:47:35 -0500

    set up skeleton react

---
+++ socialsess_front/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+const App = () => {
+     let [questions, setQuestions] = useState([])
+     return (
+          <>
+               <h1> hello universe </h1>
+          </>
+     )
+}
+
+export default App
